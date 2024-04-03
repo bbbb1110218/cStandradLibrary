25ea02a525b8e725f7a2739c30da07e13cdc176d
Mac_M2 <Mac_M2@fake.com>
2024-04-03 15:53:27 +0800

    iso646

---
+++ iso646/iso646.c
@@ -0,0 +1,30 @@
+#include <iso646.h>
+#include <stdio.h>
+
+/**
+ *  @headfile iso646.h
+ * 
+ *  @param 只是定义了一些宏 没啥用
+ *          #ifndef __cplusplus
+            #define and    &&
+            #define and_eq &=
+            #define bitand &
+            #define bitor  |
+            #define compl  ~
+            #define not    !
+            #define not_eq !=
+            #define or     ||
+            #define or_eq  |=
+            #define xor    ^
+            #define xor_eq ^=
+            #endif
+ *      
+*/
+
+int main(){
+
+    printf("1 bitand 3 =%d\n",1 bitand 3);
+     printf("1 bitor 2 =%d\n",1 bitor 2);
+    return 0;
+
+}
