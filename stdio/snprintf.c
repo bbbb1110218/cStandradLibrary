2e101b84d74a2db45315945606c649b357d5f401
Mac_M2 <Mac_M2@fake.com>
2024-04-03 22:03:18 +0800

    snprintf 按照format 取ap值

--- stdio/snprintf.c
+++ stdio/snprintf.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
+
 
 
 
 /**
- * @brief: snprintf(des,max_size,"format",...)
+ * @brief: int snprintf(des,max_size,"format",...);
+ * 
+ * @return finally strlen(des);
 */
 
-typedef struct Foo{
-    char *buff;
-    size_t size;
-}Foo;
+
+void ShowFormat(const char *format,...){
+    va_list ap;
+    va_start(ap,format);
+    char Buff[BUFSIZ]; //缓存
+
+    //n为buff占用的字符个数
+    size_t n=vsnprintf(Buff,BUFSIZ,format,ap);
+    
+    //标记一下最后一个字符
+    Buff[n]='\0';
+    va_end(ap);
+    
+    //put it to stdout
+    puts(Buff);
+
+}
 
 
-char Buff[1023];
+
+
 int main(){
-    Foo foo={.buff="hello kitty",.size=5};
+#if defined(__APPLE__)  || defined(__APPLE__)
+    ShowFormat("My name is %s,age =%d,feet = %.5f","xigua",32,24.5f);
+#else  
+    puts("not defined __apple__");
+#endif 
+    return 0;
 
-
-    snprintf(Buff,foo.size*20,"%s %s",foo.buff,"endof");
-
-    printf("%s",Buff);
 }
